ad383c21b4b8a387d7f87a672839bae0602d7f34
Javier Sanchez Fernandez <sanchezfdez.javier@gmail.com>
2021-02-23 21:52:51 +0100

    Prefiltering successfully mlinked with ground filtering

--- src/src/scripts/input_and_prefilter.py
+++ src/src/scripts/input_and_prefilter.py
@@ -9,9 +9,8 @@ def callback_print_points(msg):
     #xyz_array = ros_numpy.point_cloud2.pointcloud2_to_xyz_array(msg)
     record_array = ros_numpy.point_cloud2.pointcloud2_to_array(msg)
     scaled_array = record_array.copy()
-    #scaled_array['x'] = scaled_array['x']*0
-    scaled_array = scaled_array[scaled_array['z'] > -0.18]
-    scaled_array = scaled_array[scaled_array['x'] < 8]
+    scaled_array = scaled_array[scaled_array['x'] < 15]
+    scaled_array = scaled_array[scaled_array['x'] > 0]
     scaled_array = scaled_array[scaled_array['y'] < 4]
     scaled_array = scaled_array[scaled_array['y'] > -4]
     pc2_to_record_to_pc2 = ros_numpy.point_cloud2.array_to_pointcloud2(scaled_array, frame_id='velodyne')
@@ -25,7 +24,7 @@ if __name__ == '__main__':
     rospy.init_node('lidar')
 
     bag_subscriber = rospy.Subscriber("/velodyne_points", PointCloud2, callback_print_points)
-    bag_publisher = rospy.Publisher("/filtered_points", PointCloud2, queue_size=1)
+    bag_publisher = rospy.Publisher("/prefiltered_points", PointCloud2, queue_size=1)
 
     rate = rospy.Rate(10)
     rospy.spin()
