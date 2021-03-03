e82d64b26fa5e1018b164d15e5573a21d3a4dc55
Javier Sanchez Fernandez <sanchezfdez.javier@gmail.com>
2021-03-03 18:52:07 +0100

    Clustering first version working(only console)

--- src/src/scripts/clustering.py
+++ src/src/scripts/clustering.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+import rospy
+from sensor_msgs.msg import PointCloud2
+import ros_numpy as rnp
+
 import numpy as np
 
 from sklearn.cluster import DBSCAN
@@ -7,50 +11,43 @@ from sklearn import metrics
 from sklearn.datasets import make_blobs
 from sklearn.preprocessing import StandardScaler
 
-# Generate sample data
-centers = [[2, 2], [0, -0.5], [-3, -3], [0, -1], [-3, 5], [-1, -1]]
-X, labels_true = make_blobs(n_samples=750, centers=centers, cluster_std=0.6, random_state=0)
-X = StandardScaler().fit_transform(X)
-
-#Compute DBSCAN
-
-db  = DBSCAN(eps=0.3, min_samples=10).fit(X)
-core_samples_mask = np.zeros_like(db.labels, dtype=bool)
-core_samples_mask[db.core_sample_indices_] = True
-labels = db.labels_
-
-# Number of clusters in labels, ignoring noise if present
-
-n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-n_noise_ = list(labels).count(-1)
-
-print('Estimated number of clusters: %d' % n_clusters_)
-print('Estimated number of noise points: %d' % n_noise_)
-print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-print("Adjusted Rand Index: %0.3f" % metrics.adjusted_rand_score(labels_true, labels))
-print("Adjusted Mutual Information: %0.3f" % metrics.adjusted_mutual_info_score(labels_true, labels))
-print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(X, labels))
-
-# Plot result
 import matplotlib.pyplot as plt
 
-# Black removed and is used for noise instead.
-unique_labels = set(labels)
-colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
-for k, col in zip(unique_labels, colors):
-    if k == -1:
-        # Black used for noise.
-        col = [0, 0, 0, 1]
+def remove_field_num(a, i):
+    names = list(a.dtype.names)
+    new_names = names[:i] + names[i+1:]
+    b = a[new_names]
+    return b
 
-    class_member_mask = (labels == k)
+def callback_view_message(msg):
+    raw_points = rnp.point_cloud2.pointcloud2_to_array(msg)
+    new_points = remove_field_num(raw_points, 2)
+    array = new_points.view(np.float32).reshape(new_points.shape + (-1,))
+    rospy.loginfo(array.shape)
+    rospy.loginfo(new_points.dtype.names)
+    return array
 
-    xy = X[class_member_mask & core_samples_mask]
-    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col), markeredgecolor='k', markersize=14)
 
-    xy = X[class_member_mask & ~core_samples_mask]
-    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col), markeredgecolor='k', markersize=6)
+def callback_clustering(msg):
+    rospy.loginfo("Message received")
+    raw_points = callback_view_message(msg)
+    #Compute DBSCAN
+    db = DBSCAN(eps=0.4, min_samples=3).fit(raw_points)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    labels = db.labels_
 
-plt.title('Estimated number of clusters: %d' % n_clusters_)
-plt.show()
+    # Number of clusters in labels, ignoring noise if present
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise_ = list(labels).count(-1)
+
+    print('Estimated number of clusters: %d' % n_clusters_)
+    print('Estimated number of noise points: %d' % n_noise_)
+
+if __name__ == '__main__':
+    rospy.init_node('clustering')
+    bag_subscriber = rospy.Subscriber("/ground_segmentation/obstacle_cloud", PointCloud2, callback_clustering)
+#   bag_publisher = rospy.Publisher("clustered_points", PointCloud2, queue_size=1)
+
+    rate = rospy.Rate(10)
+    rospy.spin()
