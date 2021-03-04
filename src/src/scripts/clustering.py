15325df6d85ae2697cf4012f3d350c7668f66658
Javier Sanchez Fernandez <sanchezfdez.javier@gmail.com>
2021-03-04 10:54:50 +0100

    Clustering minor progress

--- src/src/scripts/clustering.py
+++ src/src/scripts/clustering.py
@@ -19,18 +19,15 @@ def remove_field_num(a, i):
     b = a[new_names]
     return b
 
-def callback_view_message(msg):
+def preprocessing(msg):
     raw_points = rnp.point_cloud2.pointcloud2_to_array(msg)
     new_points = remove_field_num(raw_points, 2)
     array = new_points.view(np.float32).reshape(new_points.shape + (-1,))
-    rospy.loginfo(array.shape)
-    rospy.loginfo(new_points.dtype.names)
     return array
 
 
-def callback_clustering(msg):
-    rospy.loginfo("Message received")
-    raw_points = callback_view_message(msg)
+def clustering(msg):
+    raw_points = preprocessing(msg)
     #Compute DBSCAN
     db = DBSCAN(eps=0.4, min_samples=3).fit(raw_points)
     core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
@@ -41,12 +38,31 @@ def callback_clustering(msg):
     n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
     n_noise_ = list(labels).count(-1)
 
-    print('Estimated number of clusters: %d' % n_clusters_)
-    print('Estimated number of noise points: %d' % n_noise_)
+    idxs_by_cluster = {i:[] for i in range(n_clusters_)}
+    print(idxs_by_cluster)
+
+    for idx, l in enumerate(labels):
+        if(l != -1):
+            idxs_by_cluster[l].append(idx)
+
+    clusters_keys = idxs_by_cluster.keys()
+
+    cone_idx_pseudopositions = []
+    # Pick the last index in the cluster --> Higher height point?
+    for k in clusters_keys:
+        cone_idx_pseudopositions.append(idxs_by_cluster[k][-1])
+
+    print(idxs_by_cluster)
+    print(cone_idx_pseudopositions)
+
+    # Number of points in cluster
+    #print(len(idxs_by_cluster[n_clusters_ -1]))
+
+    #print(idxs_by_cluster)
 
 if __name__ == '__main__':
     rospy.init_node('clustering')
-    bag_subscriber = rospy.Subscriber("/ground_segmentation/obstacle_cloud", PointCloud2, callback_clustering)
+    bag_subscriber = rospy.Subscriber("/ground_segmentation/obstacle_cloud", PointCloud2, clustering)
 #   bag_publisher = rospy.Publisher("clustered_points", PointCloud2, queue_size=1)
 
     rate = rospy.Rate(10)
