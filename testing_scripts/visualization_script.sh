b2d11686645ccf3ce45b6ab6989e213c3d8fdc13
Javier Sánchez Fernández <sanchezfdez.javier@gmail.com>
2021-04-20 11:25:20 +0200

    Feat: lidar launchfile working(outputs warning)

--- testing_scripts/visualization_script.sh
+++ testing_scripts/visualization_script.sh
@@ -1,9 +1,12 @@
-s
 #!/bin/bash
 
-echo  "Tools for visualization started"
+echo  "Start"
 roslaunch lidar_nodes lidar.launch &
+sleep 3
 roslaunch velodyne_pointcloud VLP16_points.launch &
+sleep 3
 rosrun rviz rviz -f velodyne &
+sleep 3
 rosrun rqt_graph rqt_graph &
-echo "Ended"
+sleep 3
+echo "Finish"
