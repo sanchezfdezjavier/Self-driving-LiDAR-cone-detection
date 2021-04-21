9c22368c74388cb61d770fbbdfe860289e0d98d7
Javier Sánchez Fernández <sanchezfdez.javier@gmail.com>
2021-04-21 12:21:25 +0200

    Feat: visualization script. Opens automatically rviz and rqt graph, ready to use

--- testing_scripts/open_visualization_environment.sh
+++ testing_scripts/open_visualization_environment.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 echo  "Start"
-roslaunch lidar_nodes lidar.launch &
-sleep 3
-roslaunch velodyne_pointcloud VLP16_points.launch &
-sleep 3
 rosrun rviz rviz -f velodyne &
 sleep 3
 rosrun rqt_graph rqt_graph &
