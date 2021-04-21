d2f4f18c17dfb45d453e42c332a3bcca95f23d11
Javier Sánchez Fernández <sanchezfdez.javier@gmail.com>
2021-04-21 22:57:06 +0200

    Feat: perception topic, timestamp saved, minor code refactoring

--- src/lidar_nodes/src/clustering.py
+++ src/lidar_nodes/src/clustering.py
@@ -4,6 +4,9 @@ import rospy
 from sensor_msgs.msg import PointCloud2
 from visualization_msgs.msg import MarkerArray
 from visualization_msgs.msg import Marker
+from geometry_msgs.msg import PoseArray
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Point
 import ros_numpy as rnp
 
 import numpy as np
@@ -16,7 +19,36 @@ from sklearn.datasets import make_blobs
 from sklearn.preprocessing import StandardScaler
 import random
 
-def get_marker(timestamp, id, x, y, z, r, g, b, a):
+
+# TODO: Finish, z? how to remove starray
+def get_PoseArray(idxs_by_cluster, nparray_obstacle_xy, starray_obstacle_xyz, timestamp):
+
+    clusters_keys = idxs_by_cluster.keys()
+
+    pose_array = PoseArray()
+
+    pose_array.header.stamp = timestamp
+    pose_array.header.frame_id = "/velodyne"
+
+    for _, idx in enumerate(clusters_keys):
+        pose = Pose()
+
+        # Cone position
+        pose.position.x = nparray_obstacle_xy[idxs_by_cluster[idx][-1]][0]
+        pose.position.y = nparray_obstacle_xy[idxs_by_cluster[idx][-1]][1]
+        pose.position.z = starray_obstacle_xyz['z'][idxs_by_cluster[idx][-1]]
+
+        # Not useful
+        # pose.orientation.w = 0
+        # pose.orientation.x = 0
+        # pose.orientation.y = 0
+        # pose.orientation.z = 0
+
+        pose_array.poses.append(pose)
+
+    return pose_array
+
+def get_cone_marker(timestamp, id, x, y, z, r, g, b, a):
     lidar_marker = Marker()
     lidar_marker.header.frame_id = "/velodyne"
     lidar_marker.header.stamp = timestamp
@@ -39,6 +71,9 @@ def get_marker(timestamp, id, x, y, z, r, g, b, a):
     return lidar_marker
 
 def get_LiDAR_marker(timestamp):
+    '''
+     Returns the marker representing the LiDAR itself in RVIZ
+    '''
     lidar_marker = Marker()
     lidar_marker.header.frame_id = "/velodyne"
     lidar_marker.header.stamp = timestamp
@@ -60,21 +95,24 @@ def get_LiDAR_marker(timestamp):
 
     return lidar_marker
 
-    
-def publish_markers(idxs_by_cluster, nparray_obstacle_xy, starray_obstacle_xyz):
+# TODO: Efficiency can be improved by better params
+def publish_markers_to_RVIZ(idxs_by_cluster, nparray_obstacle_xy, starray_obstacle_xyz):
     clusters_keys = idxs_by_cluster.keys()
     marker_array = MarkerArray()
     timestamp = rospy.Time.now()
     marker_array.markers.append(get_LiDAR_marker(timestamp))
 	
-    marker_array.markers.append(get_marker(timestamp, 100, 5, 0, 0, 1, 0, 0, 0.5))
-    marker_array.markers.append(get_marker(timestamp, 101, 10, 0, 0, 1, 0, 0, 0.5))
-    marker_array.markers.append(get_marker(timestamp, 111, 8, 0, 0, 1, 0, 0, 0.5))
-    marker_array.markers.append(get_marker(timestamp, 102, 0, 5, 0, 1, 0, 0, 0.5))
-    marker_array.markers.append(get_marker(timestamp, 103, 0, -5, 0, 1, 0, 0, 0.5))
-    marker_array.markers.append(get_marker(timestamp, 104, 5, 0, 0, 1, 0, 0, 0.5))
+    marker_array.markers.append(get_cone_marker(timestamp, 100, 5, 0, 0, 1, 0, 0, 0.5))
+    marker_array.markers.append(get_cone_marker(timestamp, 101, 10, 0, 0, 1, 0, 0, 0.5))
+    marker_array.markers.append(get_cone_marker(timestamp, 111, 8, 0, 0, 1, 0, 0, 0.5))
+    marker_array.markers.append(get_cone_marker(timestamp, 102, 0, 5, 0, 1, 0, 0, 0.5))
+    marker_array.markers.append(get_cone_marker(timestamp, 103, 0, -5, 0, 1, 0, 0, 0.5))
+    marker_array.markers.append(get_cone_marker(timestamp, 104, 5, 0, 0, 1, 0, 0, 0.5))
 
+    # id: iteration element -> cluster key
+    # idx: iteration index 
     for id, idx in enumerate(clusters_keys):
+        # TODO: Generate marker standalone function
         marker = Marker()
         marker.header.frame_id = "/velodyne"
         marker.header.stamp = timestamp
@@ -94,29 +132,34 @@ def publish_markers(idxs_by_cluster, nparray_obstacle_xy, starray_obstacle_xyz):
         marker.pose.position.x = nparray_obstacle_xy[idxs_by_cluster[idx][-1]][0]
         marker.pose.position.y = nparray_obstacle_xy[idxs_by_cluster[idx][-1]][1]
         marker.pose.position.z = 0
-        #rospy.loginfo(starray_obstacle_xyz[idx]['z'])
 
         marker_array.markers.append(marker)
 
-    publisher_rviz_markers.publish(marker_array) 
-    
+    publisher_rviz_markers.publish(marker_array)  # Publishes detected cones to RVIZ
 
 def remove_field_num(a, i):
+    '''Removes field from numpy structured array'''
     names = list(a.dtype.names)
     new_names = names[:i] + names[i+1:]
     return a[new_names]
 
 def preprocessing(msg):
+    ''''''
+    timestamp = msg.header.stamp
+
     starray_obstacle_xyz = rnp.point_cloud2.pointcloud2_to_array(msg)
     starray_obstacle_xy = remove_field_num(starray_obstacle_xyz, 2)
     nparray_obstacle_xy = starray_obstacle_xy.view(np.float32).reshape(starray_obstacle_xy.shape + (-1,))
-    return nparray_obstacle_xy, starray_obstacle_xyz
+    return timestamp, nparray_obstacle_xy, starray_obstacle_xyz
 
+# TODO: Finish
+def publish_to_perception(pose_array):
+    publisher_perception.publish(pose_array)
 
 def clustering(msg):
-    nparray_obstacle_xy, starray_obstacle_xyz = preprocessing(msg)
+    timestamp, nparray_obstacle_xy, starray_obstacle_xyz = preprocessing(msg)
 
-    #Compute DBSCAN
+    # Compute DBSCAN
     db = DBSCAN(eps=0.3, min_samples=5).fit(nparray_obstacle_xy)
     core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
     core_samples_mask[db.core_sample_indices_] = True
@@ -134,21 +177,23 @@ def clustering(msg):
 
     clusters_keys = idxs_by_cluster.keys()
     cone_idx_pseudopositions = []
+
     # Pick the last index in the cluster --> Higher height point?
     for k in clusters_keys:
         cone_idx_pseudopositions.append(idxs_by_cluster[k][-1])
 
-    # aqui esta el puto fallo joder
-    publish_markers(idxs_by_cluster, nparray_obstacle_xy, starray_obstacle_xyz)
+    # TODO: At starray_obstacle_xyz can be dropped to improve efficiency
+    publish_markers_to_RVIZ(idxs_by_cluster, nparray_obstacle_xy, starray_obstacle_xyz)
+    publish_to_perception(get_PoseArray(idxs_by_cluster, nparray_obstacle_xy, starray_obstacle_xyz, timestamp))
 
     # Number of points in cluster
-    #n_points_cluster = len(idxs_by_cluster[n_clusters_ -1])
-
+    # n_points_cluster = len(idxs_by_cluster[n_clusters_ -1])
 
 if __name__ == '__main__':
     rospy.init_node('clustering')
+
     subscriber_obstacle_cloud = rospy.Subscriber("/ground_segmentation/obstacle_cloud", PointCloud2, clustering)
     publisher_rviz_markers = rospy.Publisher('visualization_marker_array', MarkerArray, queue_size=1)
-#   bag_publisher = rospy.Publisher("clustered_points", PointCloud2, queue_size=1)
+    publisher_perception = rospy.Publisher("clustered_points", PoseArray, queue_size=1)
 
     rospy.spin()
--- src/lidar_nodes/src/input_and_prefilter.py
+++ src/lidar_nodes/src/input_and_prefilter.py
@@ -5,19 +5,22 @@ import ros_numpy
 from sensor_msgs.msg import PointCloud2
 
 def prefilter(msg):
-    rospy.loginfo("Message received")
     #xyz_array = ros_numpy.point_cloud2.pointcloud2_to_xyz_array(msg)
+    timestamp = msg.header.stamp
+
     record_array = ros_numpy.point_cloud2.pointcloud2_to_array(msg)
+ 
+    # TODO: Efficiency improvements
     scaled_array = record_array.copy()
     scaled_array = scaled_array[scaled_array['x'] < 8]
     scaled_array = scaled_array[scaled_array['x'] > 0]
     scaled_array = scaled_array[scaled_array['y'] < 4]
     scaled_array = scaled_array[scaled_array['y'] > -4]
     scaled_array = scaled_array[scaled_array['z'] < 0.5]
-    pc2_to_record_to_pc2 = ros_numpy.point_cloud2.array_to_pointcloud2(scaled_array, frame_id='velodyne')
-    bag_publisher.publish(pc2_to_record_to_pc2)
-    #rospy.loginfo(msg)
 
+    pc2_to_record_to_pc2 = ros_numpy.point_cloud2.array_to_pointcloud2(scaled_array, stamp=timestamp, frame_id='velodyne')
+
+    bag_publisher.publish(pc2_to_record_to_pc2) 
 
 
 if __name__ == '__main__':
@@ -26,7 +29,6 @@ if __name__ == '__main__':
     bag_subscriber = rospy.Subscriber("/velodyne_points", PointCloud2, prefilter)
     bag_publisher = rospy.Publisher("/prefiltered_points", PointCloud2, queue_size=1)
 
-    rate = rospy.Rate(10)
     rospy.spin()
 
 
